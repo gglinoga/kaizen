250a0b6c263a4874270f39de57a8b8c053e71506
erikchan103 <erik.chan@sugarmade.com>
2019-05-14 11:28:33 -0700

    add description footer and lessons components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar.js';
+import Description from './components/Description/Description.js';
+import Footer from './components/Footer/Footer.js'
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Description/>
+      <Footer/>
     </div>
   );
 }
---
+++ client/src/components/Description/Description.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Description.css"
+
+const Description = props =>(
+    <div>
+        <p>test</p>
+    </div>
+);
+
+export default Description;
