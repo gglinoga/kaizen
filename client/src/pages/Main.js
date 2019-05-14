09d494eebcae341a513190d699b0a48485a89897
erikchan103 <erik.chan@sugarmade.com>
2019-05-14 13:49:47 -0700

    add pages still need route

---
+++ client/src/pages/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.js';
+import Navbar from '../components/Navbar/Navbar.js';
+import Description from '../components/Description/Description.js';
+import Footer from '../components/Footer/Footer.js'
+import Lessons from '../components/Lessons/Lessons';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Description/>
+      <Footer/>
+      <Lessons/>
+    </div>
+  );
+}
+
+export default App;
