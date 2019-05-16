ee0d2c2ce8bd2d51d069d656550104f8a9ef5896
Katherine <katherinejhwang@gmail.com>
2019-05-16 11:10:42 -0700

    routes and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar.js';
-import Description from './components/Description/Description.js';
 import Footer from './components/Footer/Footer.js';
-import Lessons from './components/Lessons/Lessons';
+import Home from '../src/components/pages/Home.js';
+import Lesson from '../src/components/pages/Lesson.js';
+import LessonComplete from '../src/components/pages/LessonComplete.js'
+import User from '../src/components/pages/User.js'
 
-
-let puppies = './images/puppies.jpeg';
 
 const style = {
   backgroundColor: "#ee940d",
@@ -14,14 +15,16 @@ const style = {
 
 function App() {
   return (
+    <Router>
     <div style={style}>
       <Navbar/>
-      <Description/>
-      <Lessons
-        name
-        />
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/Lesson" component={Lesson}/>
+      <Route exact path="/LessonComplete" component={LessonComplete}/>
+      <Route exact path="/User" component={User}/>
       <Footer/>
     </div>
+    </Router>
   );
 }
 
