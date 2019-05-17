1b71655e4632d67442387f9570a9c13bbfb6b90b
Katherine <katherinejhwang@gmail.com>
2019-05-17 11:49:40 -0700

    edits to lesson & learning & home

--- client/src/components/Learning/Learning.js
+++ client/src/components/Learning/Learning.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+const style = {
+    courseName: {
+        fontFamily: "'Questrial', sans-serif",
+        textAlign: "center"
+    }
+}
+
 const Learning = props => (
     <div className="header">
         <div>
-            <h1 className="courseName">{props.courseName}</h1>
+            <h2 className="courseName" style={style.courseName}>{props.courseName}</h2>
             <img className="lessonPic" src={props.lessonPic} alt=""></img>
             <p className="lessonMaterial">{props.lessonMaterial}</p>
             <form>
@@ -12,6 +19,7 @@ const Learning = props => (
                 <input type="radio" name="quiz" value={props.choice1} /> {props.choice1} <br />
                 <input type="radio" name="quiz" value={props.choice2} /> {props.choice2} <br />
                 <input type="radio" name="quiz" value={props.choice3} /> {props.choice3} <br />
+                <p>  </p>
                 <input type="submit" value="Submit" />
             </form>
         </div>
