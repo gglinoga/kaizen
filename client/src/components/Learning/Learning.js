9f76dbe2213c466b624d637815b1ec50e82a6bad
erikchan103 <erik.chan@sugarmade.com>
2019-05-20 14:25:40 -0700

    radio works and render is not failing

--- client/src/components/Learning/Learning.js
+++ client/src/components/Learning/Learning.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 const style = {
 
@@ -21,36 +21,67 @@ const style = {
     },
 }
 
-const Learning = props => (
-    <div className="container" className="header">
-        <div className="row">
-        <div className="courseName" style={style.courseName}>{props.courseName}</div>
-        </div>
-        <div className="row">
-            <div className="col-6">
-            <p className="lessonMaterial" style={style.lessonMaterial}>{props.lessonMaterial}</p>
+class Learning extends Component {
+    state = {
+        choices: [],
+        selected: null
+    }
+
+    handleChange = (selected) => {
+        this.setState({ selected }, () => {
+            console.log(this.state);
+        });
+    }
+
+    handleSubmit = () => {
+        this.state.selected === this.props.correctAnswer ? this.handleCorrect() : this.handleWrong();
+    }
+
+    handleWrong = () => {
+        alert("u wrong")
+    }
+
+    handleCorrect = () => {
+        alert("u correct")
+        document.getElementById("nextPage").style.display="inline"; // no
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="courseName" style={style.courseName}>{this.props.courseName}</div>
+                </div>
+                <div className="row">
+                    <div className="col-6">
+                        <p className="lessonMaterial" style={style.lessonMaterial}>{this.props.lessonMaterial}</p>
+                    </div>
+                    <div className="col-6">
+                        <img className="lessonPic" style={style.img} src={this.props.lessonPic} alt=""></img>
+                    </div>
+                </div>
+                <hr></hr>
+                <div className="row">
+                    <div className="col">
+
+                            <h4>Quiz</h4>
+                            <p><b>{this.props.quizQuestion}</b></p>
+                            {this.props.choices.map((choice, idx) => {
+                                return (
+                                    <div>
+                                        <input type="radio" name="quiz" value={choice} onClick={() => this.handleChange(choice)} />
+                                        <label>{choice}</label><br />
+                                    </div>
+                                );
+                            })}
+                            <p></p>
+                            <input type="submit" onClick={this.handleSubmit} value="Submit" />
+                            <p></p>
+                    </div>
+                </div>
             </div>
-            <div className="col-6">
-            <img className="lessonPic" style={style.img} src={props.lessonPic} alt=""></img>
-            </div>
-        </div>
-        <hr></hr>
-        <div className="row">
-            <div className="col">
-            <form>
-                <h4>Quiz</h4>
-                <p><b>{props.quizQuestion}</b></p>
-                <input type="radio" name="quiz" value={props.choice0} /> {props.choice0}<br />
-                <input type="radio" name="quiz" value={props.choice1} /> {props.choice1} <br />
-                <input type="radio" name="quiz" value={props.choice2} /> {props.choice2} <br />
-                <input type="radio" name="quiz" value={props.choice3} /> {props.choice3} <br />
-                <p></p>
-                <input type="submit" value="Submit" />
-                <p></p>
-            </form>
-            </div>
-            </div>
-        </div>
-);
+        );
+    }
+}
 
 export default Learning;
--- client/src/components/pages/Lesson.js
+++ client/src/components/pages/Lesson.js
@@ -20,6 +20,10 @@ const style = {
 
     row1: {
         height: "20px",
+    },
+
+    lessonChange: {
+        display: "none",
     }
 
 }
@@ -33,30 +37,32 @@ class Lesson extends Component {
         lessonPic: ["https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg?w=730&crop=1", "https://images.code.org/ac720d8ae4d6380fc72c8d6659910bcf-image-1446407286955.png"],
         lessonMaterial: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elit ipsum, commodo id mauris et, ullamcorper dignissim urna. Nullam sit amet ligula sit amet mi vestibulum tristique. Nunc iaculis, ipsum eu scelerisque viverra, tortor lorem efficitur felis, quis porta mi nisi eget nunc. Sed accumsan semper diam id porttitor."],
-        quizzes: [{
-            question: "What is an array?",
-            choice: [" Lorem ipsum dolor sit amet", " Lorem ipsum dolor sit amet", " Lorem ipsum dolor sit amet", " Lorem ipsum dolor sit amet"],
-            correctAnswer: "c"
-        },
-        {
-            question: "Test Question 2?",
-            choice: ["1", "2", "3", "4"],
-            correctAnswer: "1"
-        }
+        quizzes: [
+            {
+                question: "What is an array?",
+                choices: ["Lorem ipsum", "Lorem ipsum dolor", "Lorem ipsum dolor sit", "Lorem ipsum dolor sit amet"],
+                correctAnswer: "Lorem ipsum dolor sit amet"
+            },
+            {
+                question: "Test Question 2?",
+                choices: ["1", "2", "3", "4"],
+                correctAnswer: "1"
+            }
         ]
     };
 
+
     updateLesson = () => {
         if (this.state.i < this.state.lessonNames.length - 1) {
-            console.log(this)
-            let counter = this.state.i;
-            counter++;
-            this.setState({ i: counter });
-            console.log(this.state.i)
+            this.setState({ i: this.state.i + 1 }, () => {
+                console.log(this.state)
+                document.getElementById("nextPage").style.display = "none"; // no
+            });
         }
     };
 
     render() {
+        console.log(this.state.quizzes[this.state.i].choices)
         return (
             <div>
                 <Navbar courseTitle={this.state.courseName} />
@@ -67,18 +73,17 @@ class Lesson extends Component {
                         <div className="col-2">
                         </div>
                         <div className="col-8">
+
                             <Learning
                                 courseName={this.state.lessonNames[this.state.i]}
                                 lessonPic={this.state.lessonPic[this.state.i]}
                                 lessonMaterial={this.state.lessonMaterial[this.state.i]}
                                 quizQuestion={this.state.quizzes[this.state.i].question}
-                                choice0={this.state.quizzes[this.state.i].choice[0]}
-                                choice1={this.state.quizzes[this.state.i].choice[1]}
-                                choice2={this.state.quizzes[this.state.i].choice[2]}
-                                choice3={this.state.quizzes[this.state.i].choice[3]}
+                                correctAnswer={this.state.quizzes[this.state.i].correctAnswer}
+                                choices={this.state.quizzes[this.state.i].choices}
                             >
                             </Learning>
-                            <button onClick={this.updateLesson}>Next Page</button></div>
+                            <button id="nextPage" onClick={this.updateLesson} style={style.lessonChange}>Next Page</button></div>
                     </div>
                 </div>
             </div>
